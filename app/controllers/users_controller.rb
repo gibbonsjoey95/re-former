a0aeab4602fb7aac186288573fe0689c83bfc905
Joey Gibbons <gibbonsjoey95@gmail.com>
2023-06-26 21:12:08 -0500

    edited

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  private
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
